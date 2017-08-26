efea9357b472b9f7138d3a4d9cf2c1c49363e23d
mba2014early <mba2014early@example.com>
2017-08-26 13:46:44 +0900

    scaffold Skill

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826044108) do
+ActiveRecord::Schema.define(version: 20170826044528) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title", default: "", null: false
     t.text "detail", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "skill", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

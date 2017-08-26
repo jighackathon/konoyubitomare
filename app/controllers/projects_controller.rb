d6c8421d53118f5c94d6124216ba76c85daf85ca
thirtythirty <qawsedr443@gmail.com>
2017-08-26 17:30:46 +0900

    コメント表示する　シード追加

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,8 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @chat_messages = ChatMessage.all
+    
   end
 
   # GET /projects/new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.new(email: 'user@example.com', password: 'password')
+
+project = Project.new(title: '小学生向けライバル探し', detail: '年齢を入力するとその年齢以下のコンテストで優秀な成績を残し得ている子の情報を一覧表示する。')
+s1 = Skill.new(skill: 'rails')
+s2 = Skill.new(skill: 'css')
+s3 = Skill.new(skill: 'html')
+s4 = Skill.new(skill: 'javascript')
+
+ProjectSkill.new(project_id: project.id, s1)
+ProjectSkill.new(project_id: project.id, s2)
+ProjectSkill.new(project_id: project.id, s3)
+ProjectSkill.new(project_id: project.id, s4)
+
